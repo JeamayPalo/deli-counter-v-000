c6d86e039becd2aab8c5bae3d6a5307e284ed427
Jeamay Palo <jeamay.palo@gmail.com>
2018-02-11 03:27:03 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,10 +2,11 @@
 def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
-  else 
+  else
     new_line = "The line is currently:"
     katz_deli.each.with_index(1) do |person, index|
       new_line << " #{index}. #{person}"
     end
     puts new_line
   end
+end
