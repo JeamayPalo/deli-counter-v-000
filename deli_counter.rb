5cc849b5d383387857f684da7440066e568e9a1a
Jeamay Palo <jeamay.palo@gmail.com>
2018-02-11 03:26:54 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,11 @@
 # Write your code here.
+def line(katz_deli)
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else 
+    new_line = "The line is currently:"
+    katz_deli.each.with_index(1) do |person, index|
+      new_line << " #{index}. #{person}"
+    end
+    puts new_line
+  end
